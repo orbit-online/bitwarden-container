7723913294701bd065c272df028dbfbfe6c86301
Anders Ingemann <aim@orbit.online>
2023-11-21 15:48:13 +0100

    shorten preamble

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 bitwarden_cli_build() {
-  set -eo pipefail
-  shopt -s inherit_errexit
-  local pkgroot
-  pkgroot=$(upkg root "${BASH_SOURCE[0]}")
+  set -eo pipefail; shopt -s inherit_errexit
+  local pkgroot; pkgroot=$(upkg root "${BASH_SOURCE[0]}")
 
   DOCKER_BUILDKIT=1 docker build --tag "secoya/bitwarden-cli:latest" "$pkgroot"
 }
